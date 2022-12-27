ffe3d2e5d9215e158a624cfa15b802761e4cfa76
Kennypapa <daramolakehinde339@gmail.com>
2022-12-26 16:57:52 -0800

    recent

---
+++ d-collections/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
